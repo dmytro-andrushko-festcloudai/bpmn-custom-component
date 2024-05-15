52773fc558cda06a5a6546586e0fa10ad9be2841
Dmytro Andrushko <dmytro.andrushko@festcloud.ai>
2024-05-15 14:34:27 +0300

    'Add property panel

---
+++ README.md
@@ -0,0 +1,16 @@
+# BPMN custom component
+
+This is example of BPMN js mode with custom components, property panel.
+
+Example [demo](https://dmytro-andrushko-festcloudai.github.io/bpmn-custom-component/)
+
+## Install package dependencies
+npm install
+
+## Start project localy
+npm start
+
+## Build project
+npm build
+
+Build file will be on **/dist** folder
--- src/index.js
+++ src/index.js
@@ -1,23 +1,31 @@
 import BpmnModeler from "bpmn-js/lib/Modeler";
-import diagramXML from '../resources/diagram.bpmn';
-import customModule from './customElement';
+import {
+  BpmnPropertiesPanelModule,
+  BpmnPropertiesProviderModule,
+} from "bpmn-js-properties-panel";
+import diagramXML from "../resources/diagram.bpmn";
+import customModule from "./customElement";
 
 import "bpmn-js/dist/assets/diagram-js.css";
 import "bpmn-js/dist/assets/bpmn-js.css";
 
 import "bpmn-js/dist/assets/bpmn-font/css/bpmn-embedded.css";
+import "@bpmn-io/properties-panel/assets/properties-panel.css";
 
 import "./assets/style/main.scss";
 
-const containerEl = document.getElementById("root");
-
 // create modeler
 const bpmnModeler = new BpmnModeler({
-  container: containerEl,
+  container: "#canvas",
   additionalModules: [
-    customModule
+    customModule,
+    BpmnPropertiesPanelModule,
+    BpmnPropertiesProviderModule,
   ],
+  propertiesPanel: {
+    parent: "#properties",
+  },
 });
 
 // import XML
-bpmnModeler.importXML(diagramXML).then(() => {})
+bpmnModeler.importXML(diagramXML).then(() => {});
